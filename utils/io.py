c00eff7ebfc3d72f4ee2d5f7783dc3e6f1b75da7
ADiBariya <adiiibariya@gmail.com>
2025-05-25 14:39:43 +0530

    Update io.py

--- utils/io.py
+++ utils/io.py
@@ -1,1 +1,8 @@
+import time
+import re
+
+def generate_image_name(prompt):
+    safe_prompt = re.sub(r"[^a-zA-Z0-9]+", "_", prompt.lower())[:40]
+    timestamp = time.strftime("%Y%m%d_%H%M%S")
+    return f"{safe_prompt}_{timestamp}.png"
 
