88a8db111bf69a04b3364e73afc7abf3430378c9
ADi <adiiibariya@gmail.com>
2026-07-11 21:10:59 +0530

    Update config.py

--- model/config.py
+++ model/config.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-# model/config.py
+
 OUTPUT_DIR = "outputs"
-MODEL_PATH = "C:\Users\ahira\BUZZ-AI\models\AnythingXL_xl.safetensors"
+MODEL_PATH = ""
 #i am not using this model path if you want to use it with config than do changes according it!!
